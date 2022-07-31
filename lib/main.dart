cc4ca04c9dda0e2221fdba3232baaf3dd0c1f898
dabuzida <dabuzida@gmail.com>
2022-07-31 14:42:15 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -23,32 +23,73 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
         // body: const Navigatorr(),
-        body: NewWidget(),
+        body: FirstPage(),
+        // body: NewWidget(),
       ),
     );
   }
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
+// class NewWidget extends StatelessWidget {
+//   const NewWidget({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: ElevatedButton(
+//         child: Text('go to second page'),
+//         onPressed: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (BuildContext context) {
+//                 return Navigatorr();
+//               },
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('go to second page'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return Navigatorr();
-              },
-            ),
-          );
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Second page'),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('First page'),
+          onPressed: () {},
+        ),
       ),
     );
   }
